e686b3c777c3b153e4bcaf0b9de1e5d7d013e007
Ojasvin Kirpane <okirp@MacBook-Pro-2.local>
2022-05-02 03:47:08 +0530

    corrected path

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,13 +24,13 @@
 	<meta name="twitter:text:title" content="Ojasvin's Portfolio!" />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:image" content="https://ojasvin.me/assets/site-banner.jpg" />
+	<meta name="twitter:image" content="https://ojasvin.me/assets/screen-banner.jpg" />
 
 	<!-- Open Graph -->
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="/" />
 	<meta property="og:title" content="Ojasvin's Portfolio" />
 	<meta property="og:description" content="Ojasvin's Portfolio!" />
-	<meta property="og:image" content="https://ojasvin.me/assets/site-banner.jpg" />
+	<meta property="og:image" content="https://ojasvin.me/assets/screen-banner.jpg" />
 	<meta property="og:image:alt" content="Ojasvin Portfolio!" />
 </svelte:head>
