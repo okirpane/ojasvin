afbdb2078c73c0994ee94221bb9349c54c2b5f45
Ojasvin Kirpane <okirp@MacBook-Pro-2.local>
2022-05-02 03:56:25 +0530

    tags fixed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,6 +14,7 @@
 	<!-- Primary tags -->
 	<title>Ojasvin's Portfolio</title>
 	<meta name="title" content="Ojasvin's Portfolio" />
+	<meta name="description" content="I'm a designer who codes." />
 	<meta
 		name="robots"
 		content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
@@ -22,7 +23,7 @@
 	<!-- Twitter -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:text:title" content="Ojasvin's Portfolio!" />
-
+	<meta name="twitter:text:description" content="I'm a designer who codes." />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:image" content="https://ojasvin.me/assets/screen-banner.jpg" />
 
@@ -30,7 +31,7 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="/" />
 	<meta property="og:title" content="Ojasvin's Portfolio" />
-	<meta property="og:description" content="Ojasvin's Portfolio!" />
+	<meta property="og:description" content="I'm a designer who codes." />
 	<meta property="og:image" content="https://ojasvin.me/assets/screen-banner.jpg" />
 	<meta property="og:image:alt" content="Ojasvin Portfolio!" />
 </svelte:head>
